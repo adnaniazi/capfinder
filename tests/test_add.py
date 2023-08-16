9fdb78e37f07ab627d16fe8ff4cb67ea9e8cb825
adnaniazi <adnaniazi@gmail.com>
2023-08-16 12:03:14 +0200

    added more tests

--- tests/test_add.py
+++ tests/test_add.py
@@ -1,10 +1,14 @@
-import pytest
 from capfinder.add import add
 
 
-def test_add_function()-> None:
+def test_add_function() -> None:
     result = add(1, 2, 3)
     assert result == 6, "Incorrect sum"
+    result = add(1, 3, 3)
+    assert result == 7, "Incorrect sum"
+    # more test for 100% coverage
+    result = add(1, 3, 5)
+    assert result == 9
 
 
 if __name__ == "__main__":
